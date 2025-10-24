ac780da90070ec9998f07ec5bc397db3dbb3c733
edmar-zx <contato.edmarcozer756@gmail.com>
2025-10-23 20:42:21 -0400

    Adicionando middleware

---
+++ middleware.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import jwt from "jsonwebtoken";
+
+const SECRET = process.env.JWT_SECRET!;
+
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get("token")?.value;
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  try {
+    // Verifica se o token é válido
+    jwt.verify(token, SECRET);
+    return NextResponse.next();
+  } catch (err) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+}
+
+export const config = {
+  matcher: ["/admin/:path*"],
+};
