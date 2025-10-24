06a1f0341d4253bbe85c80f648fb0da316ce2d40
edmar-zx <contato.edmarcozer756@gmail.com>
2025-10-23 21:48:39 -0400

    Arrumando Middleware

--- middleware.ts
+++ middleware.ts
@@ -5,21 +5,20 @@ import jwt from "jsonwebtoken";
 const SECRET = process.env.JWT_SECRET!;
 
 export function middleware(req: NextRequest) {
-  // 🔒 Em produção, verifica o token
-  if (process.env.NODE_ENV === "production") {
-    const token = req.cookies.get("authToken")?.value;
-    if (!token) return NextResponse.redirect(new URL("/", req.url));
+  const token = req.cookies.get("authToken")?.value;
+  console.log("Cookie authToken:", token);
 
-    try {
-      jwt.verify(token, SECRET);
-      return NextResponse.next();
-    } catch {
-      return NextResponse.redirect(new URL("/", req.url));
-    }
+  if (!token) {
+    return NextResponse.redirect(new URL("/", req.url));
   }
 
-  // 🚧 Em dev, deixa passar
-  return NextResponse.next();
+  try {
+    // Verifica se o token é válido
+    jwt.verify(token, SECRET);
+    return NextResponse.next();
+  } catch (err) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
 }
 
 export const config = {
